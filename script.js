2c99b49515f69bb46a0fdb5bc3c3d634bdebc3bb
Taufik Sumara <83862887+tofik-007@users.noreply.github.com>
2022-12-21 11:13:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var breedImage = $("#breed-image");
+var dropdown = $("#dog-breeds");
+var allowSubmit = true;
+var breed;
+
+$.get("https://dog.ceo/api/breeds/list/all", function (data, status) {
+    let dogBreeds = data.message;
+    for (let breed in dogBreeds) {
+        dropdown.append('<option value="' + breed + '">' + breed + '</option>');
+    }
+});
+
+dropdown.change(function () {
+    allowSubmit = true;
+});
+
+$("form button").click(function (e) {
+    e.preventDefault();
+
+    if (allowSubmit) {
+        breed = dropdown.val();
+        displayDog(breed);
+        allowSubmit = false;
+    }
+
+});
+
+$("#next a").click(function (e) {
+    e.preventDefault();
+    if (breed !== undefined) {
+        displayDog(breed);
+    }
+});
+
+function displayDog(breed) {
+    let url = "https://dog.ceo/api/breed/" + breed + "/images/random";
+
+    $("#breed-image img").remove();
+
+    $.get(url, function (data, status) {
+        let imageUrl = data.message;
+        breedImage.append('<img src="' + imageUrl + '" alt="' + breed + '">');
+    });
+
+}
